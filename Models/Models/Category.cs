b43457070531a8f9472a5ea123437cf49d29121a
Hamza <hamzamouddakur@gmail.com>
2024-02-21 19:16:19 +0100

    remove files

--- Models/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace Data.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-
-    }
-}
--- Models/Models/SubCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace Data.Models
-{
-    public class SubCategory
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public Category? category { get; set; }
-    }
-}
