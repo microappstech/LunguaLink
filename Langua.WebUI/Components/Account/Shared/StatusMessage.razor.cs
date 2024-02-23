61f3f60e23f6701da5803bb19ae46f2ee5a5d535
Hamza <hamzamouddakur@gmail.com>
2024-02-23 22:51:46 +0100

    Separate Component code and code behind

---
+++ Langua.WebUI/Components/Account/Shared/StatusMessage.razor.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Langua.WebUI.Components.Account.Shared
+{
+    public partial class StatusMessage
+    {
+        private string? messageFromCookie;
+
+        [Parameter]
+        public string? Message { get; set; }
+
+        [CascadingParameter]
+        private HttpContext HttpContext { get; set; } = default!;
+
+        private string? DisplayMessage => Message ?? messageFromCookie;
+
+        protected override void OnInitialized()
+        {
+            messageFromCookie = HttpContext.Request.Cookies[IdentityRedirectManager.StatusCookieName];
+
+            if (messageFromCookie is not null)
+            {
+                HttpContext.Response.Cookies.Delete(IdentityRedirectManager.StatusCookieName);
+            }
+        }
+    }
+}
