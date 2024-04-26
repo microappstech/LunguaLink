c86e9f6e25f56eca00a627d66242f64f34b22d1b
Hamza <hamzamouddakur@gmail.com>
2024-04-26 21:33:23 +0100

    push 2 : 26_04_2024

--- Langua.WebUI/__Data/_ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Langua.WebUI.__Data
-{
-    public class _ApplicationDbContext(DbContextOptions<_ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
